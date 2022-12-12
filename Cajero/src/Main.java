f5cc7604e0cfe5a538c483d488af03d1489d7b03
profeRSG <rsangal205@g.educaand.es>
2022-12-12 19:59:53 +0100

    Fix de nombre de variable
    asdasdasdasdasdascx

--- Cajero/src/Main.java
+++ Cajero/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		int opcion;
-		int NuevaVariableWW;
+		int nuevaVariableWW;
 		double saldo=0;
 		do {
 			mostrarMenu();//Muestra el menu principal
